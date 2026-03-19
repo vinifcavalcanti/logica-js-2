30dbf37c5b8681db36dec915d76a9645e5ae4d80
Vinícius  Fuentes Cavalcanti <142112837+vinifcavalcanti@users.noreply.github.com>
2026-03-19 19:28:20 -0300

    .

--- desafios/desafio1/js-curso-2-aula1/app.js
+++ desafios/desafio1/js-curso-2-aula1/app.js
@@ -1,9 +1,29 @@
 let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+titulo.innerHTML = 'Hora do Desafio';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 function verificarChute() {
     console.log('O botão foi clicado!')
+}
+
+function exibirMensagemNoConsole() {
+    console.log('O botão foi clicado!')
+}
+
+function exibirAlerta(){
+    alert('Eu amo Js')
+}
+
+function exibirPrompt(){
+    let nomeDaCidade = prompt('Digite o nome de uma cidade do Brasil que você gosta muito:')
+    alert(`Estive em ${nomeDaCidade} e lembrei de você.`)
+}
+
+function somandoDoisNumeros(){
+    let primeiroNumero = parseInt(prompt('Digite o primeiro número'));
+    let segundoNumero = parseInt(prompt('Digite o segundo número'));
+    let resultado = primeiroNumero + segundoNumero;
+    alert(`${primeiroNumero} + ${segundoNumero} = ${resultado}`)
 }
