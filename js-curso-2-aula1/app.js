5f3ba98ee41b313eed335e808e60a9dcf5484f96
Vinícius  Fuentes Cavalcanti <142112837+vinifcavalcanti@users.noreply.github.com>
2026-03-19 21:45:07 -0300

    Update app.js

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -6,10 +6,11 @@ function exibirTextoNaTela(tag, texto){
 }
 
 exibirTextoNaTela('h1', 'Jogo do número secreto.');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
-    console.log(numeroSecreto);
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
 }
 
 function gerarNumeroAleatorio() {
